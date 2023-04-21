747d0ea9ebf3b2330ffa4f416f447f5365074ab2
emattiza <emattiza@gmail.com>
2023-04-20 23:10:02 -0500

    add github release

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,24 @@ use chardetng::EncodingDetector;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn detect(a: &[u8]) -> &str {
+fn detect_rs_enc_name(a: &[u8]) -> PyResult<&str> {
     let mut detector = EncodingDetector::new();
     detector.feed(a, true);
     let encoding = detector.guess(None, true);
-    encoding.name()
+    let rust_name = encoding.name();
+
+    Ok(rust_name)
+}
+
+#[pyfunction]
+fn detect_codec(a: &[u8]) -> PyResult<PyAny> {
+    let enc_rs_name = detect_rs_enc_name(
+        a
+        )?;
+    Python::with_gil(|py| {
+        let codecs = PyModule::import(py, "codecs");
+        codecs.getattr("lookup")?.call1(enc_rs_name)?.extract()?
+    })
 }
 
 /// A Python module implemented in Rust.
