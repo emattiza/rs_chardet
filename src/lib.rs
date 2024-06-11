10860ac0118cd23713557f9464783de19df19280
emattiza <emattiza@gmail.com>
2024-06-10 19:25:16 -0500

    update: abi3-py38 migration
    migrate deprecations, switch to abi3-py38 given eol of py37

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ fn detect_rs_enc_name(a: &[u8]) -> PyResult<&str> {
 fn detect_codec(a: &[u8]) -> PyResult<PyObject> {
     let enc_rs_name = detect_rs_enc_name(a)?;
     let lookup_codec: PyResult<PyObject> = Python::with_gil(|py| {
-        let lookup_fn = py.import("codecs")?.getattr("lookup")?;
+        let lookup_fn = py.import_bound("codecs")?.getattr("lookup")?;
         let lookup_value = lookup_fn.call1((enc_rs_name,))?.into();
         Ok(lookup_value)
     });
@@ -28,7 +28,7 @@ fn detect_codec(a: &[u8]) -> PyResult<PyObject> {
 /// Chardet-NG support in python from rust
 /// Enables character set detection provided a byte sample
 #[pymodule]
-fn rs_chardet(_py: Python, m: &PyModule) -> PyResult<()> {
+fn rs_chardet(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(detect_rs_enc_name, m)?)?;
     m.add_function(wrap_pyfunction!(detect_codec, m)?)?;
